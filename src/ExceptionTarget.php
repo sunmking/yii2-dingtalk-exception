3c881bb025236ea70cad07008184b4cbd728b842
mrk-s <33918985+saviorlv@users.noreply.github.com>
2018-09-26 23:00:43 +0800

    update

--- src/ExceptionTarget.php
+++ src/ExceptionTarget.php
@@ -9,8 +9,10 @@ namespace Saviorlv\Log;
 
 use Yii;
 use yii\base\InvalidConfigException;
+use yii\log\Logger;
 use yii\log\Target;
 use yii\di\ServiceLocator;
+
 /**
  * ```php
  * 'components' => [
@@ -48,35 +50,42 @@ class ExceptionTarget extends Target
     }
 
     /**
-     * Sends log messages to specified email addresses.
+     * Sends log messages to dingtalk.
      * Starting from version 2.0.14, this method throws LogRuntimeException in case the log can not be exported.
      * @throws LogRuntimeException
      */
     public function export()
     {
-
-        if (empty($this->message['subject'])) {
-            $this->message['subject'] = 'Application Log';
-        }
         $messages = array_map([$this, 'formatMessage'], $this->messages);
         
         $dingTalk = $this->sendMsg($messages);
-//        if () {
-//            throw new LogRuntimeException('Unable to export log through email!');
-//        }
+
+        if ($dingTalk['errmsg']!=='ok') {
+            throw new LogRuntimeException('Unable to export log through DingTalk!');
+        }else{
+            Yii::trace(json_encode($dingTalk),Logger::LEVEL_INFO);
+        }
     }
 
+    /**
+     * @param $message
+     * @return mixed
+     * @throws InvalidConfigException
+     */
     public function sendMsg($message){
+
         $locator = new ServiceLocator;
         $locator->setComponents([
-          'dingTalkException' => [
-              'class' => 'Saviorlv\DingTalk\Robot;',
-              'accessToken' => $this->message['accessToken'],
-         ]
+            'robot' => [
+                'class' => 'Saviorlv\Dingtalk\Robot',
+                'accessToken' => $this->message['accessToken']
+            ],
         ]);
 
-        $robot = $locator->get('dingTalkException');
+        $robot = $locator->get('robot');
 
-        $response = $robot->sendTextMsg($message);
+        $response = $robot->sendMarkdownMsg('ww',$message);
+
+        return json_decode($response,true);
     }
 }
